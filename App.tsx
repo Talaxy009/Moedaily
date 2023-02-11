fb449feaea1a77fd91a2ca738e4074c867ec7c74
Talaxy009 <x009ba1@gmail.com>
2023-02-11 22:54:04 +0800

    🎨 格式化入口代码

--- App.tsx
+++ App.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import {
-  adaptNavigationTheme,
-  Provider as PaperProvider,
-  MD3DarkTheme,
-  MD3LightTheme,
-  MD3Theme,
+	adaptNavigationTheme,
+	Provider as PaperProvider,
+	MD3DarkTheme,
+	MD3LightTheme,
+	MD3Theme,
 } from 'react-native-paper';
 import {RecoilRoot} from 'recoil';
 import {useColorScheme} from 'react-native';
 import Icons from 'react-native-vector-icons/Ionicons';
 import {
-  NavigationContainer,
-  DefaultTheme,
-  DarkTheme,
+	NavigationContainer,
+	DefaultTheme,
+	DarkTheme,
 } from '@react-navigation/native';
 import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
 
@@ -22,59 +22,59 @@ import {darkColor, lightColor} from './src/common/color';
 
 const Tab = createMaterialBottomTabNavigator();
 const {LightTheme: NavLightTheme, DarkTheme: NavDarkTheme} =
-  adaptNavigationTheme({
-    reactNavigationLight: DefaultTheme,
-    reactNavigationDark: DarkTheme,
-  });
+	adaptNavigationTheme({
+		reactNavigationLight: DefaultTheme,
+		reactNavigationDark: DarkTheme,
+	});
 
 const HomeIcon = (p: any) => {
-  return p.focused ? (
-    <Icons name="home" color={p.color} size={26} />
-  ) : (
-    <Icons name="home-outline" color={p.color} size={26} />
-  );
+	return p.focused ? (
+		<Icons name="home" color={p.color} size={26} />
+	) : (
+		<Icons name="home-outline" color={p.color} size={26} />
+	);
 };
 
 const SettingsIcon = (p: any) => {
-  return p.focused ? (
-    <Icons name="settings" color={p.color} size={26} />
-  ) : (
-    <Icons name="settings-outline" color={p.color} size={26} />
-  );
+	return p.focused ? (
+		<Icons name="settings" color={p.color} size={26} />
+	) : (
+		<Icons name="settings-outline" color={p.color} size={26} />
+	);
 };
 
 export default function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+	const isDarkMode = useColorScheme() === 'dark';
 
-  const theme: MD3Theme = isDarkMode
-    ? {...MD3DarkTheme, colors: darkColor}
-    : {...MD3LightTheme, colors: lightColor};
-  const navTheme = isDarkMode ? NavDarkTheme : NavLightTheme;
+	const theme: MD3Theme = isDarkMode
+		? {...MD3DarkTheme, colors: darkColor}
+		: {...MD3LightTheme, colors: lightColor};
+	const navTheme = isDarkMode ? NavDarkTheme : NavLightTheme;
 
-  return (
-    <RecoilRoot>
-      <NavigationContainer theme={navTheme}>
-        <PaperProvider theme={theme}>
-          <Tab.Navigator initialRouteName="Index">
-            <Tab.Screen
-              name="Index"
-              component={IndexPage}
-              options={{
-                tabBarLabel: '首页',
-                tabBarIcon: HomeIcon,
-              }}
-            />
-            <Tab.Screen
-              name="Settings"
-              component={SettingsPage}
-              options={{
-                tabBarLabel: '设置',
-                tabBarIcon: SettingsIcon,
-              }}
-            />
-          </Tab.Navigator>
-        </PaperProvider>
-      </NavigationContainer>
-    </RecoilRoot>
-  );
+	return (
+		<RecoilRoot>
+			<NavigationContainer theme={navTheme}>
+				<PaperProvider theme={theme}>
+					<Tab.Navigator initialRouteName="Index">
+						<Tab.Screen
+							name="Index"
+							component={IndexPage}
+							options={{
+								tabBarLabel: '首页',
+								tabBarIcon: HomeIcon,
+							}}
+						/>
+						<Tab.Screen
+							name="Settings"
+							component={SettingsPage}
+							options={{
+								tabBarLabel: '设置',
+								tabBarIcon: SettingsIcon,
+							}}
+						/>
+					</Tab.Navigator>
+				</PaperProvider>
+			</NavigationContainer>
+		</RecoilRoot>
+	);
 }
