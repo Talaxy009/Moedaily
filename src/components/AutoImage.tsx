de20e244213ccc9715dca0ac0caf8260530f80c5
Talaxy009 <x009ba1@gmail.com>
2023-02-23 15:22:29 +0800

    💄 使用 ImageBackground 的 contain 模式处理长图

--- src/components/AutoImage.tsx
+++ src/components/AutoImage.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import {
 	View,
-	Image,
 	StyleSheet,
 	Dimensions,
+	ImageBackground,
 	TouchableWithoutFeedback,
 } from 'react-native';
 
@@ -15,23 +15,30 @@ interface AutoImageProps {
 	onLongPress: () => void;
 }
 
-const vw = Dimensions.get('window').width;
+// "flex: 1" 无效，因此手动获取视图宽高
+const {width, height} = Dimensions.get('window');
 
 export default function AutoImage({img, onError, onLongPress}: AutoImageProps) {
 	const url = img.urls.regular || img.urls.original || img.urls.small;
+	const [loaded, setLoaded] = React.useState(false);
 
 	return (
 		<TouchableWithoutFeedback onLongPress={onLongPress}>
-			<View style={{height: (img.height / img.width) * vw, width: vw}}>
-				<Image
-					style={styles.img}
+			<View style={{width, height}}>
+				{!loaded && (
+					<ImageBackground
+						style={styles.img}
+						resizeMode="contain"
+						source={{
+							uri: img.urls.mini,
+						}}
+					/>
+				)}
+				<ImageBackground
 					onError={onError}
-					source={{
-						uri: img.urls.mini,
-					}}
-				/>
-				<Image
 					style={styles.img}
+					resizeMode="contain"
+					onLoad={() => setLoaded(true)}
 					source={{
 						uri: url,
 					}}
