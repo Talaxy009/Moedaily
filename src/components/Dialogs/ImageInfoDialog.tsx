bbf2901bd9e4e3b2e4327cdcdbdce3a5011a873c
Talaxy009 <x009ba1@gmail.com>
2023-03-13 14:32:00 +0800

    ✨ 从作品信息中导入标签

--- src/components/Dialogs/ImageInfoDialog.tsx
+++ src/components/Dialogs/ImageInfoDialog.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
-import {Linking} from 'react-native';
-import {Button, Dialog, Portal, List} from 'react-native-paper';
+import {Linking, ScrollView, StyleSheet} from 'react-native';
+import Clipboard from '@react-native-community/clipboard';
+import {Button, Dialog, Portal, List, Chip} from 'react-native-paper';
 
+import {getTags, storageTags} from '../../common/storage';
 import {ClockIcon, AccIcon, TagIcon} from '../ListIcons';
+import {useToast} from '../../utils/hooks';
 import strings from './strings';
 
 import type {ImageData} from '../../common/types';
@@ -18,38 +21,139 @@ export default function ImageInfoDialog({
 	onClose,
 	data,
 }: DialogProps) {
+	const toast = useToast();
+	const [tagMode, setTagMode] = React.useState(false);
+	const [tags, setTags] = React.useState(new Set<string>());
+
+	const handleClose = () => {
+		onClose();
+		setTagMode(false);
+	};
+
 	const handlePixiv = () =>
 		Linking.openURL(`https://www.pixiv.net/i/${data?.pid}`);
 
+	const handleCopyUID = () => {
+		if (data) {
+			Clipboard.setString(data.uid.toString());
+			toast(strings.copied);
+		}
+	};
+
+	const handleTagsMode = () => {
+		setTags(new Set<string>());
+		setTagMode(true);
+	};
+
+	const handleSelect = (v: string) => {
+		const newTags = new Set([...tags]);
+		if (newTags.has(v)) {
+			newTags.delete(v);
+		} else {
+			newTags.add(v);
+		}
+		setTags(newTags);
+	};
+
+	const handleAddTags = () => {
+		getTags().then((v) => {
+			const n = v || new Set<string>();
+			tags.forEach((t) => n.add(t));
+			storageTags(n);
+			toast(strings.importTagsSucc);
+			handleClose();
+		});
+	};
+
 	return (
 		<Portal>
-			<Dialog visible={visible} onDismiss={onClose}>
-				<Dialog.Title>{data?.title || ''}</Dialog.Title>
+			<Dialog visible={visible} onDismiss={handleClose}>
+				<Dialog.Title>
+					{tagMode ? strings.importTags : data?.title}
+				</Dialog.Title>
 				<Dialog.Content>
-					<List.Item
-						title={strings.author}
-						description={data?.author}
-						left={AccIcon}
-					/>
-					<List.Item
-						title={strings.time}
-						left={ClockIcon}
-						description={
-							data?.uploadDate &&
-							new Date(data.uploadDate).toLocaleDateString()
-						}
-					/>
-					<List.Item
-						title={strings.tags}
-						left={TagIcon}
-						description={data?.tags.toString()}
-					/>
+					{tagMode && data ? (
+						<ScrollView
+							style={styles.list}
+							contentContainerStyle={styles.listContent}
+						>
+							{data.tags.map((v, i) => (
+								<Chip
+									key={i}
+									style={styles.chip}
+									onPress={() => handleSelect(v)}
+									icon={tags.has(v) ? 'check' : undefined}
+									mode={tags.has(v) ? 'flat' : 'outlined'}
+								>
+									{v}
+								</Chip>
+							))}
+						</ScrollView>
+					) : (
+						<>
+							<List.Item
+								left={AccIcon}
+								title={strings.author}
+								description={data?.author}
+								onPress={handleCopyUID}
+							/>
+							<List.Item
+								left={ClockIcon}
+								title={strings.time}
+								description={
+									data?.uploadDate &&
+									new Date(
+										data.uploadDate,
+									).toLocaleDateString()
+								}
+							/>
+							<List.Item
+								left={TagIcon}
+								title={strings.tags}
+								onPress={handleTagsMode}
+								description={data?.tags.toString()}
+							/>
+						</>
+					)}
 				</Dialog.Content>
 				<Dialog.Actions>
-					<Button onPress={handlePixiv}>{strings.openPixiv}</Button>
-					<Button onPress={onClose}>{strings.close}</Button>
+					{tagMode ? (
+						<>
+							<Button onPress={() => setTagMode(false)}>
+								{strings.cancel}
+							</Button>
+							<Button onPress={handleAddTags}>
+								{strings.import}
+							</Button>
+						</>
+					) : (
+						<>
+							<Button onPress={handlePixiv}>
+								{strings.openPixiv}
+							</Button>
+							<Button onPress={handleClose}>
+								{strings.close}
+							</Button>
+						</>
+					)}
 				</Dialog.Actions>
 			</Dialog>
 		</Portal>
 	);
 }
+
+const styles = StyleSheet.create({
+	list: {
+		marginBottom: 16,
+		minHeight: 64,
+		maxHeight: 128,
+	},
+	listContent: {
+		justifyContent: 'center',
+		flexDirection: 'row',
+		flexWrap: 'wrap',
+	},
+	chip: {
+		margin: 4,
+	},
+});
